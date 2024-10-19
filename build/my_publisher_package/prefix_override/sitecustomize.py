cd9503369884618d4b9cef259305b99e472fb79f
UiAbot Mini <uiabot@pi5-5.lan>
2024-10-19 17:20:48 +0200

    update odometry

--- build/my_publisher_package/prefix_override/sitecustomize.py
+++ build/my_publisher_package/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/devtex/uiabot_ws/install/my_publisher_package'
+    sys.prefix = sys.exec_prefix = '/home/uiabot/uiabot_ws/install/my_publisher_package'
